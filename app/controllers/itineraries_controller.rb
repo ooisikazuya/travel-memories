f56b739b687973bfbd2b30b6ce8133c4b865d141
ooisikazuya <i1no1su6ke@gmail.com>
2021-01-24 18:39:22 +0900

    コーディング

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,8 +1,12 @@
 class ItinerariesController < ApplicationController
   def index
+    @user = current_user
+    @groups = Group.where(id: @user.group_users.pluck(:group_id))
   end
 
   def new
+    @user = current_user
+    @groups = Group.where(id: @user.group_users.pluck(:group_id))
   end
 
   def edit
